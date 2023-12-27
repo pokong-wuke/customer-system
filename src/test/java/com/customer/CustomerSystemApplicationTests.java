0513925a5d7255248906cc6521a83608ca665162
shaokaibo <shaokaibo@qluanyun.com>
2023-12-27 18:13:48 +0800

    添加junit

--- src/test/java/com/customer/CustomerSystemApplicationTests.java
+++ src/test/java/com/customer/CustomerSystemApplicationTests.java
@@ -1,13 +1,16 @@
 package com.customer;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-//@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CustomerSystemApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CustomerSystemApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 
 }
